1e0221855f9a07605af340be55cf0f54aecd6ff3
rohanagarwal <rohan.agarwal204@gmail.com>
2023-12-09 04:30:24 +0530

    Changes for AirstackXmtp

--- adsense-fe/src/app/dapp/page.tsx
+++ adsense-fe/src/app/dapp/page.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { init } from "@airstack/airstack-react";
+import { useQuery } from "@airstack/airstack-react";
+import XmtpAirstack from '@/components/XmtpAirstack';
 
 type Props = {}
 
-const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
+init("1bbb7ff8739434ceba402c6e565fac0f4");
+
+interface QueryResponse {
+  data: Data;
+  loading: boolean;
+  error: Error;
 }
 
-export default page
+interface Data {
+  Wallet: Wallet;
+}
+
+interface Error {
+  message: string;
+}
+
+interface Wallet {
+  socials: Social[];
+  addresses: string[];
+}
+
+interface Social {
+  dappName: "lens" | "farcaster";
+  profileName: string;
+}
+
+const GET_VITALIK_LENS_FARCASTER_ENS = `
+query MyQuery {
+  Wallet(input: {identity: "vitalik.eth", blockchain: ethereum}) {
+    socials {
+      dappName
+      profileName
+    }
+    addresses
+  }
+}
+`;
+
+export default function Dapp(props: Props) {
+
+  return (
+    <div>
+      <XmtpAirstack />
+    </div>
+  )
+}
---
+++ adsense-fe/src/components/XmtpAirstack.tsx
@@ -0,0 +1,78 @@
+'use client'
+import React from 'react'
+import { init } from "@airstack/airstack-react";
+import { useQuery } from "@airstack/airstack-react";
+
+init("1bbb7ff8739434ceba402c6e565fac0f4");
+
+interface QueryResponse {
+  data: Data | null;
+  loading: boolean | null;
+  error: Error | null;
+}
+
+interface Data {
+  Wallet: Wallet;
+}
+
+interface Error {
+  message: string;
+}
+
+interface Wallet {
+  socials: Social[];
+  addresses: string[];
+}
+
+interface Social {
+  dappName: "lens" | "farcaster";
+  profileName: string;
+}
+
+const GET_VITALIK_LENS_FARCASTER_ENS = `
+query MyQuery {
+  Wallet(input: {identity: "vitalik.eth", blockchain: ethereum}) {
+    socials {
+      dappName
+      profileName
+    }
+    addresses
+  }
+}
+`;
+
+export default function XmtpAirstack() {
+  const { data, loading, error }: QueryResponse = useQuery(GET_VITALIK_LENS_FARCASTER_ENS, {}, { cache: false });
+  console.log("data", JSON.stringify(data))
+
+  return (
+    <>
+        Showing information for vitalik.eth
+       {data ? (
+        <div>
+            <p>
+                {data.Wallet.socials.map((social, i) => (
+                    <div key={i}>
+                        <p>
+                            Dapp name: {social.dappName}
+                        </p>
+                        <p>
+                            Profile name: {social.profileName}
+                        </p>
+                    </div>
+                ))}
+            </p>
+        </div>
+        ) : (
+        <div>
+            <p>
+                {loading ? "Loading..." : "Error"}
+            </p>
+            <p>
+                {error ? error.message : ""}
+            </p>
+        </div>
+        )}
+    </>
+  )
+}

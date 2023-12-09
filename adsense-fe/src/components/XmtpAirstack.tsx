9fe3b9459e8835ff501fa232c0bbf1f26593ace9
rohanagarwal <rohan.agarwal204@gmail.com>
2023-12-09 05:38:10 +0530

    Current XMTP logic

--- adsense-fe/src/components/XmtpAirstack.tsx
+++ adsense-fe/src/components/XmtpAirstack.tsx
@@ -41,9 +41,9 @@ query MyQuery {
 }
 `;
 
+
 export default function XmtpAirstack() {
   const { data, loading, error }: QueryResponse = useQuery(GET_VITALIK_LENS_FARCASTER_ENS, {}, { cache: false });
-  console.log("data", JSON.stringify(data))
 
   return (
     <>
@@ -66,7 +66,7 @@ export default function XmtpAirstack() {
         ) : (
         <div>
             <p>
-                {loading ? "Loading..." : "Error"}
+                {loading ? "Loading info for vitalik.eth..." : "Error"}
             </p>
             <p>
                 {error ? error.message : ""}
